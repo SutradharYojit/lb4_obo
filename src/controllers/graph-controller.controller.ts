2016a4780abddbc8ec97b8cbd4c34a83eb72ff49
SutradharYojit <jimmysuthar08@gmail.com>
2023-12-07 17:26:48 +0530

    add new changes

--- src/controllers/graph-controller.controller.ts
+++ src/controllers/graph-controller.controller.ts
@@ -1,69 +1,10 @@
 import * as msal from '@azure/msal-node';
 import {inject} from '@loopback/context';
-import {HttpErrors, Request, Response, RestBindings, get, requestBody} from '@loopback/rest';
+import {HttpErrors, Request, Response, RestBindings, get, post, requestBody} from '@loopback/rest';
 import {authConfig} from '../authConfig';
 import {MsalService} from '../services/msal.service'; // Assuming you have a service for MSAL operations
-import {Console} from 'console';
+import {ResponseType} from '@microsoft/microsoft-graph-client';
 
-// export class GraphController {
-//   constructor(
-//     @inject('services.MsalService')
-//     protected msalService: MsalService,
-//   ) { }
-
-//   @post('/profile')
-//   async getProfile(
-//     // @inject(RestBindings.Http.REQUEST) req: Request,
-//     // @inject(RestBindings.Http.RESPONSE) res: Response,
-//     @requestBody({
-//       content: {
-//         'application/json': {
-//           schema: {
-//             type: 'object',
-//             properties: {
-//               aud: {type: 'string'},
-//               iss: {type: 'string'},
-//               iat: {type: 'number'},
-//               nbf: {type: 'number'},
-//               exp: {type: 'number'},
-//               aio: {type: 'string'},
-//               azp: {type: 'string'},
-//               azpacr: {type: 'string'},
-//               name: {type: 'string'},
-//               oid: {type: 'string'},
-//               preferred_username: {type: 'string'},
-//               rh: {type: 'string'},
-//               scp: {type: 'string'},
-//               sub: {type: 'string'},
-//               tid: {type: 'string'},
-//               uti: {type: 'string'},
-//               ver: {type: 'string'},
-//             },
-//           },
-//         },
-//       },
-//     })
-//     body: any,
-//   ): Promise<object> {
-//     // ... Your existing logic here, adjusted for TypeScript and LoopBack 4
-//     // For example, instead of `req.get('authorization')`, use `req.headers.authorization`
-//     // Error handling will be slightly different, using `throw new HttpErrors.Unauthorized('Your message')`
-//     // Example:
-//     console.log(body);
-//     // if (!this.msalService.isAppOnlyToken(req)) {
-//     //   throw new HttpErrors.Unauthorized('This route requires a user token');
-//     // }
-
-//     // Rest of your code logic here
-
-//     return body; // Return the final response
-//   }
-// }
-// // Import necessary decorators
-// import {post, requestBody, Response, RestBindings} from '@loopback/rest';
-// import {inject} from '@loopback/core';
-
-// Define your controller class
 export class MyController {
   constructor(
     @inject('services.MsalService') private msalService: MsalService,
@@ -72,7 +13,7 @@ export class MyController {
   ) { }
 
   // POST endpoint to receive JSON data
-  @get('/profile')
+  @post('/profile')
   async processData(
     @requestBody({
       content: {
@@ -80,6 +21,7 @@ export class MyController {
           schema: {
             type: 'object',
             properties: {
+              Authorization: {type: 'string'},
               aud: {type: 'string'},
               iss: {type: 'string'},
               iat: {type: 'number'},
@@ -103,16 +45,13 @@ export class MyController {
       },
     })
     body: any, // Use 'any' or create an interface with the required properties
-  ): Promise<object> {
-    // Your logic to process the JSON data goes here
-    const authHeader = this.req.headers.authorization as string;
+  ): Promise<any> {
 
-    const accessToken = authHeader.substring('Bearer '.length);
+    const accessToken = body.Authorization;
 
-    // Now you can use accessToken as needed
-
-    console.log(body);
+    console.log(body.Authorization);
     if (this.msalService.isAppOnlyToken(body)) {
+
       throw new HttpErrors.Unauthorized('This route requires a user token');
     }
     console.log(authConfig.protectedRoutes.profile.delegatedPermissions.scopes);
@@ -122,14 +61,39 @@ export class MyController {
       try {
         const oboToken = await this.msalService.getOboToken(accessToken);
 
-        
+        // Use the on-behalf-of token to make requests to the Microsoft Graph API
+        // Use the on-behalf-of token to make requests to the Microsoft Graph API
+        const graphClient = await this.msalService.getGraphClient(oboToken);
+        const graphResponse = await graphClient.api('/me').responseType(ResponseType.RAW).get();
+        if (graphResponse.status === 401) {
+          if (graphResponse.headers.get('WWW-Authenticate')) {
+            if (this.msalService.isClientCapableOfClaimsChallenge(body)) {
+
+              this.response
+                .status(401)
+                .header('WWW-Authenticate', graphResponse.headers.get('WWW-Authenticate') || '')
+                .send({errorMessage: 'Continuous access evaluation resulted in claims challenge'});
+
+            }
+
+            this.response.status(401).send({
+              errorMessage: 'Continuous access evaluation resulted in claims challenge but the client is not capable. Please enable client capabilities and try again',
+            });
+          }
+          throw new HttpErrors.Unauthorized('Unauthorized');
+        }
+        const graphData = await graphResponse.json();
+        this.response.status(200).send(graphData);
+
+        // this.response.status(201);
+        // // Respond with a success message or any other data
+        // return body;
       }
       catch (error) {
         if (error instanceof msal.InteractionRequiredAuthError) {
           throw new HttpErrors.BadRequest('Error Occured');
         }
       }
-
     }
     else {
       throw new HttpErrors.BadRequest('User does not have the required permissions');
@@ -137,8 +101,6 @@ export class MyController {
 
 
     // Change the status code to 201 (Created)
-    this.response.status(201);
-    // Respond with a success message or any other data
-    return body;
+
   }
 }
